3f1bb6fdaefd00ec1b0d12779aac778d2ff4e81a
Bmoe <bmboe001@odu.edu>
2021-12-18 16:13:41 -0500

    [Loading Photos] Implemented Loading Photos

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -9,6 +9,7 @@ import { Storage } from '@capacitor/storage';
 export class PhotoService {
 
   public photos: UserPhoto[] = [];
+  private PHOTO_STORAGE = 'photos';
   constructor() { }
 
   public async addNewToGallery() {
@@ -22,6 +23,29 @@ export class PhotoService {
     // Save the picture and add it to photo collection
     const savedImageFile = await this.savePicture(capturedPhoto);
     this.photos.unshift(savedImageFile);
+
+    Storage.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos),
+    });
+  }
+
+  public async loadSaved() {
+    // Retrieve cached photo array data
+    const photoList = await Storage.get({ key: this.PHOTO_STORAGE });
+    this.photos = JSON.parse(photoList.value) || [];
+
+    // Display the photo by reading into base64 format
+for (let photo of this.photos) {
+  // Read each saved photo's data from the Filesystem
+  const readFile = await Filesystem.readFile({
+    path: photo.filepath,
+    directory: Directory.Data,
+  });
+
+  // Web platform only: Load the photo as base64 data
+  photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+}
   }
 
   private async savePicture(photo: Photo) {
